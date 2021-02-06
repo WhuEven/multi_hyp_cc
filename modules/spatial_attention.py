3f6b5bcc65810fcdba97c26de603d360eb8b3f29
浩楠 朱 <362157541@qq.com>
2021-02-06 10:21:53 +0800

    模型1：添加空间、通道注意力，并更改卷积为3x3

---
+++ modules/spatial_attention.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)  # 输入两个通道，一个是maxpool 一个是avgpool的
+        self.sigmoid = nn.Sigmoid()
+
+   
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)  # 对池化完的数据cat 然后进行卷积
+        return self.sigmoid(x)
