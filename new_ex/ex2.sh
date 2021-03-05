f3b93a1bcd21567dc84e6269c331567df1174b2d
浩楠 朱 <362157541@qq.com>
2021-03-05 15:57:14 +0800

    成功的新模型，ex2训练测试

---
+++ new_ex/ex2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#2021.3.5,新模型的table1-pre训练测试
+TRAINED_MODEL_PATH="./new_ex_output/ex2/"
+GPU_ID="0"
+
+CUDA_VISIBLE_DEVICES="$GPU_ID" python3 hold_out.py new_ex/ex2_step1.json Multidataset nus_cube+ data/multidataset/nus_cube+/all.txt -gpu 0 --outputfolder $TRAINED_MODEL_PATH
+
+CUDA_VISIBLE_DEVICES="$GPU_ID" python3 crossvalidation.py new_ex/ex2_step2.json data/shi_gehler/all.txt -gpu 0 --outputfolder $TRAINED_MODEL_PATH --pretrainedmodel $TRAINED_MODEL_PATH/Multidataset/nus_cube+/kmeans_finalaffine_noconf/0/model_best.pth.tar
