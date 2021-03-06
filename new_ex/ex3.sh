f3e38f558c6c9a9e2135442c79becf12506c8d4a
浩楠 朱 <362157541@qq.com>
2021-03-06 11:59:56 +0800

    my_model ex3 table2

---
+++ new_ex/ex3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#在NUS数据集上的结果
+
+# Declare an array of string with type
+declare -a CAMERAS=("canon_eos_1D_mark3" "canon_eos_600D" "fuji" "nikonD5200" "panasonic" "olympus" "sony" "samsung" )
+
+TRAINED_MODEL_PATH="./new_ex_output/ex3/"
+GPU_ID="0"
+
+# Iterate the string array using for loop
+for camera in ${CAMERAS[@]}; do
+   CUDA_VISIBLE_DEVICES="$GPU_ID" python3 crossvalidation.py new_ex/ex3.json data/nus/$camera.txt -gpu 0 --outputfolder $TRAINED_MODEL_PATH/$camera/
+done
